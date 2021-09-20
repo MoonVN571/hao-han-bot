230df1df63ddd7a83394cb00527f890ef4d24805
MoonVNN <60611504+MoonVN571@users.noreply.github.com>
2021-09-20 19:08:46 +0700

    Add info command

---
+++ slashCommands/info/info.js
@@ -0,0 +1,30 @@
+module.exports = {
+    name: "info",
+    description: "Xem cấu hình của server",
+
+    async execute(client, interaction, args) {
+        interaction.reply({embeds: [{
+            title: "SERVER INFO",
+            description: "Thông tin Hosting của server.",
+            fields: [
+                {
+                    name: "CPU",
+                    value: "Intel Xeon Platium 8124M @ 3.00Ghz",
+                    inline: false,
+                },
+                {
+                    name: "MEMORY",
+                    value: "8GB Ram",
+                    inline: false
+                },
+                {
+                    name: "DISK",
+                    value: "5GB",
+                    inline: false
+                }
+            ],
+            color: "GREEN",
+            timestamp: new Date()
+        }], allowedMentions: { repliedUser: false } });
+    }
+}

754300accba99c2629b28261a724b0054a6722bf
MoonVNN <60611504+MoonVN571@users.noreply.github.com>
2021-09-20 19:25:44 +0700

    Update botinfo.js

--- commands/info/botinfo.js
+++ commands/info/botinfo.js
@@ -8,6 +8,7 @@ module.exports = {
         let temp = parseInt(os.uptime());
 
         let day = hours = 0, minutes = 0, seconds = 0;
+        day = parseInt(((temp /86400)));
         hours = parseInt(((temp - day * 86400) / 3600));
         minutes = parseInt(((temp - day * 86400 - hours * 3600)) / 60);
         seconds = parseInt(temp % 60);
